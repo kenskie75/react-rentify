0e8350fb85127ba578b0693114fdb43042181baa
chappy <lumangyao112@gmail.com>
2023-12-03 23:13:59 +0800

    fix

--- src/context/LoadingContext/LoadingContext.tsx
+++ src/context/LoadingContext/LoadingContext.tsx
@@ -25,7 +25,7 @@ export default function LoadingContextProvider(props:Props){
     function handleCloseLoading(){
         setTimeout(() => {
             setLoading(false);
-        },5000);
+        },2000);
        
     }
 
